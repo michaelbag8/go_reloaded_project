d5bd3afc8b5eeb33ab12bf66592c7549e439a7db
michaelbag8 <michaelbag8@gmail.com>
2026-04-14 00:04:10 +0100

    fix: errors

--- article.go
+++ article.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"strings"
+)
+
+func fixArticle(word string) string {
+	words := strings.Fields(word)
+	vowels := "aeiuohAEIUOH"
+	for i := 0; i < len(words); i++ {
+		if (words[i] == "A" || words[i] == "a") && strings.ContainsRune(vowels, rune(words[i+1][0])) {
+			if words[i] == "A" {
+				words[i] = "An"
+			} else {
+				words[i] = "an"
+			}
+		}
+	}
+	return strings.Join(words, " ")
+}
