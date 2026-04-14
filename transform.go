00b5d13a260f7bc9a1a582b5ac3aaa6d5f212e49
michaelbag8 <michaelbag8@gmail.com>
2026-04-14 12:27:02 +0100

    fix: markers function

--- transform.go
+++ transform.go
@@ -6,13 +6,16 @@ import (
 	"unicode"
 )
 
-func capitalize(word string) string {
+func Capitalize(word string) string {
 	words := strings.Fields(word)
-	for i, char := range words {
-		runes := []rune(strings.ToLower(char))
-		runes[0] = unicode.ToUpper(runes[0])
+	for i, currentWord := range words {
+		runes := []rune(strings.ToLower(currentWord))
+		if len(runes) > 0 {
+			runes[0] = unicode.ToUpper(runes[0])
+		}
 		words[i] = string(runes)
 	}
+
 	return strings.Join(words, " ")
 }
 
@@ -47,33 +50,49 @@ func ConvertNumbers(word string) string {
 	return strings.Join(words, " ")
 }
 
-func ApplyMarkers(word string) string {
-	words := strings.Fields(word)
-	for i := 0; i < len(words); i++ {
-		switch words[i] {
-		case "(low)":
-			if i > 0 {
-				words[i-1] = strings.ToLower(words[i-1])
-			}
-			words = append(words[:i], words[i+1:]...)
-			i--
+func markerDetector(word, mode string) string {
+	switch mode {
+	case "low":
+		return strings.ToLower(word)
+	case "up":
+		return strings.ToUpper(word)
+	case "cap":
+		return Capitalize(word)
+	}
+	return word
+}
 
-		case "(up)":
-			if i > 0 {
-				words[i-1] = strings.ToUpper(words[i-1])
-			}
-			words = append(words[:i], words[i+1:]...)
-			i--
-		case "(cap)":
-			if i > 0 {
-				capitalize(words[i-1])
+func applyLastN(word string) string {
+	word = strings.ReplaceAll(word, ", ", ",")
+	words := strings.Fields(word)
+
+	for i := 0; i < len(words); i++ {
+		if strings.HasPrefix(words[i], "(") && strings.HasSuffix(words[i], ")") {
+			inner := strings.Trim(words[i], "()")
+			extract := strings.Split(inner, ",")
+			mode := extract[0]
+
+			if len(extract) == 1 {
+				if i > 0 {
+					words[i-1] = markerDetector(words[i-1], mode)
+				}
+			} else {
+				num, err := strconv.Atoi(extract[1])
+				if err == nil {
+					start := i - num
+					if start < 0 {
+						start = 0
+					}
+					for j := start; j < i; j++ {
+						words[j] = markerDetector(words[j], mode)
+					}
+				}
+
 			}
 			words = append(words[:i], words[i+1:]...)
 			i--
 
 		}
-
 	}
-
 	return strings.Join(words, " ")
 }
